9864ac622c086c4e3369ecae3f6006203bf0e576
Stolz <stolz@gsmlandia.com>
2012-05-28 20:57:00 +0200

    Removed unused param

--- application/controllers/test_gettext.php
+++ application/controllers/test_gettext.php
@@ -5,28 +5,29 @@ class Test_gettext extends CI_Controller {
 	/**
 	 * Controller to test gettext installation
 	 */
-	public function index($lang = 'es_ES')
+	public function index()
 	{
 		$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
 
 		echo 'You must first run the Gettext scripts in the language folder',"<br/>\n";
 		echo "<hr/>\n";
 
-		//If your code is ASCII (English) comment out the next block and run the script gettext_ascii.sh
+		/* If your code is ASCII (English) comment out the next block and run the script gettext_ascii.sh */
 		echo "Original: Hello, how are you?<br/>\n";
 		$this->_set_lang('es_ES');
 		echo 'Spanish: ',_('Hello, how are you?'),"<br/>\n";
 		$this->_set_lang('en_US');
 		echo 'English: ',_('Hello, how are you?'),"<br/>\n";
 
-		//ONLY ONE BLOK SHOULD BE COMMENTED OUT
+		//ONLY ONE BLOCK SHOULD BE COMMENTED OUT
 
-		//If your code is UTF-8 (Spanish, French, etc) comment out the next block and run the script gettext_utf8.sh
+		/* If your code is UTF-8 (Spanish, French, etc) comment out the next block and run the script gettext_utf8.sh */
 // 		echo "Original: Hola, ¿cómo estás?<br/>\n";
 // 		$this->_set_lang('es_ES');
 // 		echo 'Spanish: ',_('Hola, ¿cómo estás?'),"<br/>\n";
 // 		$this->_set_lang('en_US');
 // 		echo 'English: ',_('Hola, ¿cómo estás?'),"<br/>\n";
+
 
 		echo "<hr/>\n";
 		echo 'If the translation is not working check if the laguage is available in your system (locale -a) and/or restart your web server';
