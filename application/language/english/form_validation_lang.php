306fc12e040c2cde630c46811e82025a1da83be6
Stolz <stolz@gsmlandia.com>
2012-06-11 15:25:05 +0200

    Native CI messages translated to Spanish via Gettext

---
+++ application/language/english/form_validation_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$lang['required']			= _('The %s field is required.');
+$lang['isset']				= _('The %s field must have a value.');
+$lang['valid_email']		= _('The %s field must contain a valid email address.');
+$lang['valid_emails']		= _('The %s field must contain all valid email addresses.');
+$lang['valid_url']			= _('The %s field must contain a valid URL.');
+$lang['valid_ip']			= _('The %s field must contain a valid IP.');
+$lang['min_length']			= _('The %s field must be at least %s characters in length.');
+$lang['max_length']			= _('The %s field can not exceed %s characters in length.');
+$lang['exact_length']		= _('The %s field must be exactly %s characters in length.');
+$lang['alpha']				= _('The %s field may only contain alphabetical characters.');
+$lang['alpha_numeric']		= _('The %s field may only contain alpha-numeric characters.');
+$lang['alpha_dash']			= _('The %s field may only contain alpha-numeric characters, underscores, and dashes.');
+$lang['numeric']			= _('The %s field must contain only numbers.');
+$lang['is_numeric']			= _('The %s field must contain only numeric characters.');
+$lang['integer']			= _('The %s field must contain an integer.');
+$lang['regex_match']		= _('The %s field is not in the correct format.');
+$lang['matches']			= _('The %s field does not match the %s field.');
+$lang['is_unique'] 			= _('The %s field must contain a unique value.');
+$lang['is_natural']			= _('The %s field must contain only positive numbers.');
+$lang['is_natural_no_zero']	= _('The %s field must contain a number greater than zero.');
+$lang['decimal']			= _('The %s field must contain a decimal number.');
+$lang['less_than']			= _('The %s field must contain a number less than %s.');
+$lang['greater_than']		= _('The %s field must contain a number greater than %s.');
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./application/language/english/form_validation_lang.php */
---
+++ application/language/english/migration_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['migration_none_found']			= _("No migrations were found.");
+$lang['migration_not_found']			= _("This migration could not be found.");
+$lang['migration_multiple_version']		= _("This are multiple migrations with the same version number: %d.");
+$lang['migration_class_doesnt_exist']	= _("The migration class \"%s\" could not be found.");
+$lang['migration_missing_up_method']	= _("The migration class \"%s\" is missing an 'up' method.");
+$lang['migration_missing_down_method']	= _("The migration class \"%s\" is missing an 'down' method.");
+$lang['migration_invalid_filename']		= _("Migration \"%s\" has an invalid filename.");
+
+
+/* End of file migration_lang.php */
+/* Location: ./application/language/english/migration_lang.php */
