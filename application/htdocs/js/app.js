07990153348e07480772d6b001ebf3930d1b6fc5
Stolz <stolz@gsmlandia.com>
2012-09-13 13:51:29 +0200

    Updated Zurb Foundation framework to latest version

--- application/htdocs/js/app.js
+++ application/htdocs/js/app.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
 	// It's pecific to jQuery.
 	// Occurs first, after the HTML-Document is loaded and DOM is ready.
+
+	//Zurb Foundation
+	$(document).foundationMediaQueryViewer();
+	$(document).foundationAlerts();
+	$(document).foundationAccordion();
+	$(document).tooltips();
+	$('input, textarea').placeholder();
+	$(document).foundationButtons();
+	$(document).foundationNavigation();
+	$(document).foundationCustomForms();
+	$(document).foundationTabs({callback:$.foundation.customForms.appendCustomMarkup});
+
 });
 
 $(window).load(function() {
 	// It's a standard event in the DOM.
 	// Occurs later, after all content (e.g. images,frames,...) are fully loaded.
+
+	$(".do-orbit").orbit();
 });
--- application/views/template.php
+++ application/views/template.php
@@ -8,28 +8,40 @@
 	<title><?= $title ?> | <?= PROJECT_NAME ?></title>
 	<base href="<?= config_item('base_url') ?>" />
 
+	<!-- Meta -->
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="description" content="to-do set your description" />
 	<meta name="author" content="Stolz to-do set your name" />
 	<link type="text/plain" rel="author" href="<?= $assets ?>humans.txt" />
-	<!--to-do favicon-->
 
-	<?php if($is_mobile AND ! $foundation) : ?>
+	<!-- Favicon -->
+	<!-- to-do -->
+
+	<!-- Mobile -->
+	<?php if($is_mobile) : ?>
 	<meta name="HandheldFriendly" content="True" /><!--BlackBerry-->
 	<meta name="MobileOptimized" content="960" /><!--Windows Mobile-->
 	<meta http-equiv="cleartype" content="on" /><!--Windows Mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /><!--Webkit-->
 	<?php endif ?>
 
-	<?php if($foundation) : ?><link type="text/css" rel="stylesheet" href="<?= $assets ?>css/foundation.css" /><?php endif ?>
-	<?php if(isset($css)) foreach($css as $file) : ?>
+	<!-- CSS -->
+	<?php if($foundation) : ?>
+	<link type="text/css" rel="stylesheet" href="<?= $assets ?>css/foundation.min.css" />
+	<?php endif; if(isset($css)) foreach($css as $file) : ?>
 	<link type="text/css" rel="stylesheet" href="<?= $assets ?>css/<?= $file ?>.css" />
 	<?php endforeach ?>
 
-	<script type="text/javascript" src="<?= $assets ?>js/foundation/modernizr.foundation.js"></script>
-	<!--[if lt IE 9]><script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<!-- JavaScript -->
+	<?php if($foundation) : /* foundation.min.js includes JQuery */?>
+	<script type="text/javascript" src="<?= $assets ?>js/foundation.min.js"></script>
+	<?php else : ?>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script type="text/javascript">if (typeof jQuery == 'undefined')document.write(unescape("%3Cscript src='<?= $assets ?>js/jquery.min.js' type='text/javascript'%3E%3C/script%3E"));</script>
+	<?php endif; if(isset($js)) foreach($js as $file) : ?>
+	<script type="text/javascript" src="<?= $assets ?>js/<?= $file ?>.js"></script>
+	<?php endforeach ?>
+	<script type="text/javascript" src="<?= $assets ?>js/app.js"></script>
 </head>
 <body>
 <?php
@@ -43,16 +55,5 @@
 	if( ! $is_mobile AND ENVIRONMENT != 'production')
 		$this->output->enable_profiler(TRUE);
 ?>
-
-	<?php if($foundation) : ?>
-	<script type="text/javascript" src="<?= $assets ?>js/foundation/jquery.reveal.js"></script>
-	<script type="text/javascript" src="<?= $assets ?>js/foundation/jquery.orbit-1.4.0.js"></script>
-	<script type="text/javascript" src="<?= $assets ?>js/foundation/jquery.customforms.js"></script>
-	<script type="text/javascript" src="<?= $assets ?>js/foundation/jquery.placeholder.min.js"></script>
-	<script type="text/javascript" src="<?= $assets ?>js/foundation/jquery.tooltips.js"></script>
-	<script type="text/javascript" src="<?= $assets ?>js/foundation/app.js"></script>
-	<?php endif;if(isset($js)) foreach($js as $file) : ?>
-	<script type="text/javascript" src="<?= $assets ?>js/<?= $file ?>.js"></script>
-	<?php endforeach ?>
 </body>
 </html>
