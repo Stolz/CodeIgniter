d959795ddfb059677f19c6605fc7bd8ed4e9ab34
Stolz <stolz@gsmlandia.com>
2012-09-24 17:44:02 +0200

    Zurb foundation updated to latest version (3.1.1)

--- application/htdocs/js/app.js
+++ application/htdocs/js/app.js
@@ -1,23 +1,40 @@
-$(document).ready(function() {
-	// It's pecific to jQuery.
-	// Occurs first, after the HTML-Document is loaded and DOM is ready.
+var $doc = $(document), Modernizr = window.Modernizr;
+
+$doc.ready(function() { // It's pecific to jQuery. Occurs first, after the HTML-Document is loaded and DOM is ready.
 
 	//Zurb Foundation
-	$(document).foundationMediaQueryViewer();
-	$(document).foundationAlerts();
-	$(document).foundationAccordion();
-	$(document).tooltips();
+	$.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+	$.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+	$.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
 	$('input, textarea').placeholder();
-	$(document).foundationButtons();
-	$(document).foundationNavigation();
-	$(document).foundationCustomForms();
-	$(document).foundationTabs({callback:$.foundation.customForms.appendCustomMarkup});
+	$.fn.foundationButtons          ? $doc.foundationButtons() : null;
+	$.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+	$.fn.foundationTopBar           ? $doc.foundationTopBar({breakPoint: 940}) : null;
+// 	$.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+// 	$.fn.foundationTabs             ? $doc.foundationTabs() : null;
+	$.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
 
+	// UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+	// $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+	// $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+	// $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+	// $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+
+	//Cosmetic: Toggle class on foudnation .sub-nav before loading the link
+	$('dl.sub-nav dd').on('click.fndtn', function (event) {
+		$(this).addClass('active').siblings().removeClass('active');
+	});
+
+	// Hide address bar on mobile devices
+	if (Modernizr.touch) {
+		$(window).load(function () {
+			setTimeout(function () {
+				window.scrollTo(0, 1);
+			}, 0);
+		});
+	}
 });
 
-$(window).load(function() {
-	// It's a standard event in the DOM.
-	// Occurs later, after all content (e.g. images,frames,...) are fully loaded.
-
-	$(".do-orbit").orbit();
+$(window).load(function() { // It's a standard event in the DOM. Occurs later, after all content (e.g. images,frames,...) are fully loaded.
+	$('.do-orbit').orbit({pauseOnHover: true, startClockOnMouseOut: true, startClockOnMouseOutAfter: 0});
 });
