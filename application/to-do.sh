d71ff147459de131a0ef4cd4c2b889368ae114cc
Stolz <stolz@gsmlandia.com>
2012-10-05 14:19:47 +0200

    Grep now ignores binary files

--- application/to-do.sh
+++ application/to-do.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )" #No resolve los enlaces simbolic
 #SCRIPT_DIR="$( cd -P "$( dirname "$0" )" && pwd )" #Resuelve los enlaces simbolicos
 
 cd $SCRIPT_DIR
-grep -r --colour=auto --exclude=to-do.sh --exclude-dir=logs "to-do" .
+grep -Ir --colour=auto --exclude=to-do.sh --exclude-dir=logs "to-do" .
 
 
 
