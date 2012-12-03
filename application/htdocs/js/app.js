0bd9db2a2cf97a6728772cbecd8ee43b748447da
Stolz <stolz@gsmlandia.com>
2012-12-03 18:43:56 +0100

    Fixed JS erros when Zurb Foundation is not loaded

--- application/htdocs/js/app.js
+++ application/htdocs/js/app.js
@@ -27,41 +27,46 @@ $doc.ready(function() { // This event is specific to jQuery. Occurs first, after
 	}());
 
 	// ========= Zurb Foundation ===============
-
-	// Cosmetic: Toggle class on foudnation .sub-nav before loading the link
-	$('dl.sub-nav dd').on('click.fndtn', function (event) {
-		$(this).addClass('active').siblings().removeClass('active');
-	});
-	
-    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
-    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
-    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
-    $('input, textarea').placeholder();
-
-	// UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-	// $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-	// $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-	// $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-	// $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
-
-	// Hide address bar on mobile devices
-	if (Modernizr.touch) {
-		$(window).load(function () {
-			setTimeout(function () {
-				window.scrollTo(0, 1);
-			}, 0);
+	if(Modernizr)//If we have Modernizr then Zurb Foundation has been loaded
+	{
+		// Cosmetic: Toggle class on foudnation .sub-nav before loading the link
+		$('dl.sub-nav dd').on('click.fndtn', function (event) {
+			$(this).addClass('active').siblings().removeClass('active');
 		});
+
+		$.fn.foundationAlerts			? $doc.foundationAlerts() : null;
+		$.fn.foundationButtons			? $doc.foundationButtons() : null;
+		$.fn.foundationAccordion		? $doc.foundationAccordion() : null;
+		$.fn.foundationNavigation		? $doc.foundationNavigation() : null;
+		$.fn.foundationTopBar			? $doc.foundationTopBar() : null;
+		$.fn.foundationCustomForms		? $doc.foundationCustomForms() : null;
+		$.fn.foundationMediaQueryViewer	? $doc.foundationMediaQueryViewer() : null;
+		$.fn.foundationTabs				? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
+		$.fn.foundationTooltips			? $doc.foundationTooltips() : null;
+		$.fn.foundationMagellan			? $doc.foundationMagellan() : null;
+		$.fn.foundationClearing			? $doc.foundationClearing() : null;
+		$('input, textarea').placeholder();
+
+		// UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+		// $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+		// $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+		// $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+		// $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+
+		// Hide address bar on mobile devices
+		if(Modernizr.touch) {
+			$(window).load(function () {
+				setTimeout(function () {
+					window.scrollTo(0, 1);
+				}, 0);
+			});
+		}
 	}
 });
 
 $(window).load(function() { // This event is a standard event in the DOM. Occurs later, after all content (e.g. images,frames,...) are fully loaded.
-	$('.do-orbit').orbit({pauseOnHover: true, startClockOnMouseOut: true, startClockOnMouseOutAfter: 0});
+
+	if(Modernizr)//If we have Modernizr then Zurb Foundation has been loaded
+		$('.do-orbit').orbit({pauseOnHover: true, startClockOnMouseOut: true, startClockOnMouseOutAfter: 0});
+
 });
