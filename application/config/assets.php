e5aa2e9a8b7940cf47e4a45ec63b309d1acb8c60
Stolz <stolz@gsmlandia.com>
2013-02-06 17:19:38 +0100

    Updated jQuery and jQuery UI

--- application/config/assets.php
+++ application/config/assets.php
@@ -17,14 +17,14 @@ For 'css' and 'js', if the filename starts with 'http' then is assumed to be a r
 
 
 $config['jquery'] = array(
-	'cdn'		=> '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js',
+	'cdn'		=> '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
 	'fallback'	=> 'typeof jQuery == "undefined"',
 	'js'		=> 'jquery.min',
 );
 
 $config['jquery-ui'] = array(
-	'css'		=> 'smoothness/jquery-ui-1.9.2.custom.min',
-	'cdn'		=> array('//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js', '//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/i18n/jquery.ui.datepicker-es.js'),
+	'css'		=> 'smoothness/jquery-ui-1.10.0.custom.min',
+	'cdn'		=> array('//ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js', '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/i18n/jquery.ui.datepicker-es.js'),
 	'fallback'	=> '!window.jQuery.ui',
 	'js'		=> array('jquery-ui.min', 'jquery.ui.datepicker-es'),
 );
