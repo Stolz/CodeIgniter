903157bdd2e9839025c1f4dda1f8b56062238832
Stolz <stolz@gsmlandia.com>
2013-05-13 20:50:50 +0200

    Zurb foundation updated to version 4.1.6

--- application/htdocs/js/app.js
+++ application/htdocs/js/app.js
@@ -1,22 +1,27 @@
-var $doc = $(document);
+if(typeof jQuery != 'undefined') {
 
-// This event is specific to jQuery. Occurs first, after the HTML-Document is loaded and DOM is ready.
-$doc.ready(function() {
+	var $doc = $(document);
 
-	// Zurb Foundation
-	if(jQuery().foundation)
-		$doc.foundation();
+	// This event is specific to jQuery. Occurs first, after the HTML-Document is loaded and DOM is ready.
+	$doc.ready(function() {
 
-	// Exec pending tasks pushed in view files
-	if(typeof whenready != 'undefined' && whenready instanceof Array)
-		for (var i = 0, length = whenready.length; i < length; i++)
-			if (whenready[i] != null)
-				(whenready[i])();
+		// Zurb Foundation
+		if($.foundation)
+			$doc.foundation();
 
-});
+		// Exec pending tasks pushed in view files
+		if(typeof whenready != 'undefined' && whenready instanceof Array)
+			for (var i = 0, length = whenready.length; i < length; i++)
+				if (whenready[i] != null)
+					(whenready[i])();
+	});
 
-// This event is a standard event in the DOM. Occurs later, after all content (e.g. images, frames,...) are fully loaded.
-$(window).load(function() {
-	// Hide address bar on mobile devices
-	//setTimeout(function () {window.scrollTo(0, 1);}, 0);
-});/
+
+	// This event is a standard event in the DOM. Occurs later, after all content (e.g. images, frames,...) are fully loaded.
+	$(window).load(function() {
+		//setTimeout(function () {window.scrollTo(0, 1);}, 0); // Hide address bar on mobile devices
+	});
+}
+
+
+
